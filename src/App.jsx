4e2d169aa5912384f3a3022bab1991cf74bf08cc
richardbr95 <rejjeri25@gmail.com>
2025-07-17 16:52:55 -0300

    Start: Node modules, tailwindcss + vite

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="text-4xl font-bold text-center  text-blue-500 bg-slate-500 mt-10">
+      Tailwind funcionando! 🚀
+      <h1 className="bg-slate-400 ">deess</h1>
+    </div>
+  );
+}
